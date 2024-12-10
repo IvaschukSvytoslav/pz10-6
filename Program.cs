ef994232e534c021388ed7337140dc93d5d77159
user <user@WINDOWS-5AN5BRH>
2024-12-10 20:44:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Масив чисел
+        int[] numbers = { 2, 12, 14, 18, 25, 15 };
+
+        // Вибір трьох чисел, починаючи з другого
+        var selectedNumbers = numbers.Skip(1).Take(3);
+
+        // Виведення результатів
+        Console.WriteLine("Вибрані числа:");
+        foreach (var number in selectedNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
